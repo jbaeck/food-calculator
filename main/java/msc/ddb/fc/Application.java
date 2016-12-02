f1a2b4d17264c2043d1e169d02824fb1e6ad87c4
jbaeck <joachim.baeck@gmail.com>
2016-12-02 12:33:29 +0100

    Initial Version

---
+++ main/java/msc/ddb/fc/Application.java
@@ -0,0 +1,16 @@
+package msc.ddb.fc;
+
+/**
+ * Main Class of the FeedCalculator.
+ */
+public class Application {
+
+    /**
+     * Generates feeding plan.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("DER FUTTER-KALKULATOR");
+    }
+}
---
+++ test/java/msc/ddb/fc/LogicTest.java
@@ -0,0 +1,21 @@
+package msc.ddb.fc;
+
+import org.junit.Assert;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jb on 26.11.16.
+ */
+public class LogicTest {
+  @org.junit.Test
+  public void isResponsible() throws Exception {
+    Assert.assertEquals(1,2);
+  }
+
+  @org.junit.Test
+  public void calculate() throws Exception {
+    assertEquals(1,2);
+  }
+
+}
